66afc6c54a9f37d8133d860a6e76ac5eda18313c
ArvinoDel <andikasupriyadi27@gmail.com>
2025-02-04 12:23:00 +0700

    `Refactor form fields in Home and Profile components`

--- src/pages/dashboard/home.jsx
+++ src/pages/dashboard/home.jsx
@@ -328,23 +328,23 @@ export function Home() {
                         </div>
 
                         <div className="sm:col-span-3">
-                          <label htmlFor="country" className="block text-sm/6 font-medium text-gray-900">
-                            Country
+                          <label htmlFor="keterangan" className="block text-sm/6 font-medium text-gray-900">
+                            Keterangan
                           </label>
-                          <div className="mt-2 grid grid-cols-1">
+                          <div className="relative mt-2 w-full">
                             <select
-                              id="country"
-                              name="country"
-                              autoComplete="country-name"
-                              className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                              id="keterangan"
+                              name="keterangan"
+                              autoComplete="keterangan-name"
+                              className="peer w-full appearance-none rounded-md border border-gray-300 bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-none focus:border-indigo-600 focus:ring-2 focus:ring-indigo-600 sm:text-sm"
                             >
-                              <option>United States</option>
-                              <option>Canada</option>
-                              <option>Mexico</option>
+                              <option value="" disabled selected hidden>Pilih Keterangan</option>
+                              <option value="sakit">Izin Sakit</option>
+                              <option value="keperluan">Izin Keperluan</option>
                             </select>
                             <ChevronDownIcon
                               aria-hidden="true"
-                              className="pointer-events-none col-start-1 row-start-1 mr-2 size-1 self-center justify-self-end text-gray-500 sm:size-4"
+                              className="absolute right-3 top-1/2 w-3 h-3 -translate-y-1/2 text-gray-500 peer-focus:text-indigo-600"
                             />
                           </div>
                         </div>
@@ -398,9 +398,6 @@ export function Home() {
                 </div>
 
                 <div className="mt-6 flex items-center justify-end gap-x-6">
-                  <button type="button" className="text-sm/6 font-semibold text-gray-900">
-                    Cancel
-                  </button>
                   <button
                     type="submit"
                     className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
