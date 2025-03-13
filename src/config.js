61f928dba26108a615a8611971ce78d1c9e97777
m-faiz07 <muhammad.faiz44367@smk.belajar.id>
2025-03-13 23:52:36 +0700

    fix: Remove trailing slash from API_BASE_URL for consistency; add new profile pictures

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // API Configuration
-export const API_BASE_URL = 'https://api.muhfaz.my.id/';
+export const API_BASE_URL = 'https://api.muhfaz.my.id';
 export const UPLOADS_BASE_URL = 'https://api.muhfaz.my.id/uploads';
 
 // Other configuration values can be added here 
